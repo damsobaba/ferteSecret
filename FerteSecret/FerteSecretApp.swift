538849d8f0eea571018b76b86e50558911cc8463
Adam <adam.mabrouki@webwag.com>
2025-08-08 11:14:44 +0200

    Initial Commit

---
+++ FerteSecret/FerteSecretApp.swift
@@ -0,0 +1,17 @@
+//
+//  FerteSecretApp.swift
+//  FerteSecret
+//
+//  Created by Adam Mabrouki on 08/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FerteSecretApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FerteSecretTests/FerteSecretTests.swift
@@ -0,0 +1,16 @@
+//
+//  FerteSecretTests.swift
+//  FerteSecretTests
+//
+//  Created by Adam Mabrouki on 08/08/2025.
+//
+
+import Testing
+
+struct FerteSecretTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
