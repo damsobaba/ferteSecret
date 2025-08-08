fb83a7a38ed06872626345e2776945a72cc3d660
Adam <adam.mabrouki@webwag.com>
2025-08-08 12:16:18 +0200

    dd

--- FerteSecret/ContentView.swift
+++ FerteSecret/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, wdfdorld!")
+            Text("Hello, wdfddddorld!")
             Text("ydyd")
         }
         .padding()
